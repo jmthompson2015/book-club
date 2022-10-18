e1fc9bc9f40070fdb07bf628422cd4fee71b4d9c
Jeffrey M. Thompson <jmthompson2015@users.noreply.github.com>
2022-10-18 14:31:35 -0600

    Changed to use Prettier from the command line.

---
+++ run-eslint.sh
@@ -0,0 +1,9 @@
+
+npx eslint --fix "./artifact/Book2.js"
+npx prettier --write "./artifact/Book2.js"
+
+npx eslint --fix "./artifact/Person2.js"
+npx prettier --write "./artifact/Person2.js"
+
+npx eslint --fix "./artifact/Series2.js"
+npx prettier --write "./artifact/Series2.js"
