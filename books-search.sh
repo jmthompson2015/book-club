5b19e4fa333938c57c1d3c8380a767d4138258a8
Jeffrey M. Thompson <jmthompson2015@users.noreply.github.com>
2024-10-07 09:29:19 -0600

    Updated for 2025 first half choices.

--- books-search.sh
+++ books-search.sh
@@ -1,27 +1,14 @@
-export DCL="https://dcl.bibliocommons.com/v2/search?searchType=smart&query="
 export GR="https://www.goodreads.com/search?q="
-export IMDB="https://www.imdb.com/find?s=all&q="
-export LT="https://www.librarything.com/search.php?search="
-export WIKI="https://en.wikipedia.org/wiki/"
 
-#open ${DCL}"${1}"
-#open ${GR}"${1}"
-#open ${IMDB}"${1}"
-#open ${LT}"${1}"
-#open ${WIKI}"${1}"
-
+# 2025
 export books=( 
-'Resurrection Walk by Michael Connelly'
-'Lone Wolf by Gregg Hurwitz'
-'The Edge by David Baldacci'
-'Blind Vigil by Matt Coyle'
-'Strange Sally Diamond by Liz Nugent'
-'Dead End Girl by LT Vargus'
-'Listen for the Lie by Amy Tintera'
-'Holly by Stephen King'
-'Razorblade Tears by SA Cosby'
-'His and Hers by Alice Feeney'
-'The Last word by Elly Griffiths'
+ 'We Solve Murders by Richard Osman'
+ 'The New Couple in 5B by Lisa Unger'
+ 'Middle of the Night by Riley Sager'
+ 'Here One Moment by Liane Moriarty'
+ 'Beautiful Ugly by Alice Feeney'
+ 'The Waiting by Michael Connelly'
+ 'Everyone This Christmas Has a Secret by Benjamin Stevenson'
 )
 
 for b in "${books[@]}"; do
